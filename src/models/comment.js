4e1538d6c80809c39b7d4148468d9ff546ccc0c3
royshravan <114107861+royshravan@users.noreply.github.com>
2022-10-08 19:14:45 +0530

    Delete src directory

--- src/models/comment.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-var commentSchema = new mongoose.Schema({
-  content: String,
-  createdAt: { type: Date, default: Date.now, required: true },
-  createdBy: { type: mongoose.Schema.ObjectId, ref: "User", required: true },
-});
-
-export default mongoose.model("Comment", commentSchema);
--- src/routes/indexRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const router = express.Router();
-
-router.get("/", async (req, res) => {
-  res.json({ msg: "hi" });
-});
-
-export default router;
